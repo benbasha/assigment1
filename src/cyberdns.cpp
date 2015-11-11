b3f6417a1587dc492a79fe7c32b6ac2767953d63
basha ben <benbash@guest5x2.cs.bgu.ac.il>
2015-11-11 14:04:09 +0200

    cyberdns changes

--- src/cyberdns.cpp
+++ src/cyberdns.cpp
@@ -7,10 +7,17 @@ CyberDNS::CyberDNS(){
 
 void CyberDNS::AddPC(CyberPC & cyber_pc_) {
     CyberDNS::cyber_DNS_[cyber_pc_.getName()] = &cyber_pc_;
+    //CyberDNS::cyber_DNS_.insert( std::pair< std::string , std::string >( cyber_pc_.getName() , &cyber_pc_ ));
 }
 
 CyberPC & CyberDNS::GetCyberPC(const std::string & cyber_pc_name) const {
+    std::map<std::string, CyberPC &>::iterator it;
+    it = CyberDNS::cyber_DNS_.find(cyber_pc_name);
 
+    if (it != CyberDNS::cyber_DNS_.end())
+        return it->second;
+
+    return NULL;
 }
 
 std::vector<std::string> CyberDNS::GetCyberPCList() {// Return list of PC's as a vector of strings.
