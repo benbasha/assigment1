6bb82d0dfc337a7c9daa1a1f07bb41b561bdd911
benbasha <benbasha@gmail.com>
2015-11-19 15:44:18 +0200

    done!

---
+++ include/simulation.h
@@ -0,0 +1,16 @@
+#ifndef XML_PARSER_H
+#define XML_PARSER_H
+
+#include "cyberpc.h"
+#include "cyberexpert.h"
+
+void readComputers(CyberDNS &cyberDNS);
+
+void readNetwork(CyberDNS &cyberDNS);
+
+void simulate(CyberDNS &cyberDNS);
+
+void runSimulation(CyberDNS &cyberDNS);
+
+
+#endif
